20f1e58fe05d473a30c1d44488fee6f5486883af
AndriiKostenko84 <a.kostenko84@gmail.com>
2023-02-22 16:12:44 +0200

    task1

---
+++ src/js/task-01.js
@@ -0,0 +1,12 @@
+const categoriesEl = document.querySelector('#categories');
+
+console.log('Number of categories:', categories.children.length);
+
+
+const categoriesItems = categories.querySelectorAll('.item');
+
+categoriesItems.forEach(item => {
+    console.log('Category:', item.firstElementChild.textContent);
+    console.log('Elements:', item.lastElementChild.children.length)
+    
+});
