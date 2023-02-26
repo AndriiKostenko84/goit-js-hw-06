8d16b231c3288a00535d5d49b54e5a09c4f82c2e
AndriiKostenko84 <a.kostenko84@gmail.com>
2023-02-26 10:50:29 +0200

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const AllingredientsEl = document.querySelector('.ingredients');
+
+const elements = ingredients.map((ingredients) => {
+  const ingredientsEl = document.createElement('li');
+  ingredientsEl.textContent = ingredients;
+  ingredientsEl.classList.add('item');
+  return ingredientsEl;
+});
+console.log(elements);
+
+AllingredientsEl.append(...elements);
