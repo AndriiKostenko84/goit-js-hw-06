cc8837997c7492925c16f98f1f65c2443fd5a066
AndriiKostenko84 <a.kostenko84@gmail.com>
2023-02-27 13:17:00 +0200

    Update task-04.js

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -4,6 +4,17 @@ let step = 1;
 const counterEl = document.querySelector ('#counter');
 const valueEl = document.querySelector('#value');
 
-counterEl.firstElementChild.addEventListener('click', handleDecrementButtonEl)
-valueEl.lastElementChild.addEventListener('click', handleIncrementButtonEl);
+counterEl.firstElementChild.addEventListener('click', handleDecrementButtonEl);
+counterEl.lastElementChild.addEventListener ('click', handleIncrementButtonEl);
 
+function  handleDecrementButtonEl () {
+    counterValue =-step;
+    valueEl.textContent = counterValue;
+    console. log('Click on decrement button');
+};
+
+function handleIncrementButtonEl () {
+    counterValue += step;
+    valueEl.textContent = counterValue;
+    console. log('Click on increment button');
+}
