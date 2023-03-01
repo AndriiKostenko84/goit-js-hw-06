b83b8ee6efd0898ef765f49d5ab91757aa9566f1
AndriiKostenko84 <a.kostenko84@gmail.com>
2023-03-01 14:42:07 +0200

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
+const colorEl = document.querySelector('.color');
+const bodyEl = document.querySelector('body');
+const changeColorBtnEl = document.querySelector('.change-color');
+
+changeColorBtnEl.addEventListener('click', () => {
+bodyEl.style.backgroundColor = getRandomHexColor();
+colorEl.textContent = getRandomHexColor()
+});
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
